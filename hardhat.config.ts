827ace212583d8fad1dc592c32aed0eb7842a9f9
stanislav9505@gmail.com <stanislav9505@gmail.com>
2022-06-07 16:39:06 +0300

    Changed configuration to typescript

---
+++ hardhat.config.ts
@@ -0,0 +1,53 @@
+import { task, subtask } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
+import 'dotenv/config';
+
+/**
+ * Deployments Tasks
+ */
+task("deploy-localhost", "Deploys the bridge and one test erc20 token on localhost")
+  .addOptionalParam("name", "The token name")
+  .addOptionalParam("symbol", "The token bridge")
+  .setAction(async ({ name, symbol }, hre) => {
+    await hre.run('compile');
+    const bridgeAddress = await hre.run('deploy-bridge');
+    if (name !== undefined && symbol !== undefined) {
+      await hre.run('deploy-token', { name, symbol, bridgeAddress });
+    }
+ });
+
+subtask("deploy-token", "Deploys token on localhost")
+  .addParam("name", "The token name")
+  .addParam("symbol", "The token symbol")
+  .addOptionalParam("bridgeAddress", "The bridge to be granted minter role")
+  .setAction(async ({name, symbol, bridgeAddress}) => {
+    const deployToken = require("./scripts/deployments/deploy-token");
+    return await deployToken(name, symbol, bridgeAddress);
+ });
+
+subtask("deploy-bridge", "Deploys bridge on localhost")
+  .setAction(async () => {
+    const deployBridge = require("./scripts/deployments/deploy-bridge");
+    return await deployBridge();
+  });
+
+/**
+ * Interactions Tasks
+ */
+task("bridge-localhost", "Interact with bridge on localhost")
+  .addParam("tokenContract", "The token conrtact address")
+  .addParam("bridgeContract", "The bridge conrtact address")
+  .setAction(async (args, hre, runSuper) => {
+    const bridge = require("./scripts/interactions/bridge");
+    await bridge(args.tokenContract, args.bridgeContract);
+  });
+
+export default {
+  solidity: "0.8.4",
+  networks: {
+    ganache: {
+      url: process.env.GANACHE_URL,
+      chainId: 1337
+    }
+  }
+};
